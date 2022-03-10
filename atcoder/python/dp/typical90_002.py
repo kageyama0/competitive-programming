65e008fff85e9878b68b78e98ee83f4e9454eda7
yuki kageyama <kageyamaforwork@gmail.com>
2022-03-10 09:39:53 +0900

    typical90_002

---
+++ atcoder/python/dp/typical90_002.py
@@ -0,0 +1,26 @@
+generator object <genexpr>
+n = int(input())
+
+# nが奇数の場合、出力できるカッコはない
+if n % 2 == 1:
+    print("")
+
+# 制約がn<=20なので,21ループ
+dp = [set() for _ in range(21)]
+dp[0].add("")
+
+for i in range(2, 10, 2):
+    # https://stackoverflow.com/questions/3929278/what-does-ior-do-in-python
+    # |= : これは、setに新しい項目を追加する方法。ORの意味。
+    dp[i] |= set('(' + S + ')' for S in dp[i - 2])
+    #print("before dp[i]:",i,dp[i])
+
+    for l in range(2, i, 2):
+        r = i - l
+        dp[i] |= set(SL + SR for SL in dp[l] for SR in dp[r])
+
+    #print("after dp[i]:",i,dp[i])
+
+ANS = sorted(dp[n])
+
+print(*ANS, sep='\n')
---
+++ atcoder/python/競プロ典型90問/002.py
@@ -0,0 +1,23 @@
+n = int(input())
+
+# 現在の文字:s
+# 使える左括弧の数:i
+# 使える右括弧の数:j
+def dfs(s,left,right):
+
+    # 左括弧と右括弧を使い切った場合に、出力
+    if left == right == 0:
+        print(s)
+        return
+
+    # 左括弧は足す場合
+    if left > 0:
+        dfs(s + '(', left-1, right)
+
+    # 右括弧を足す場合
+    # ただし、左括弧のほうが右括弧よりも使われていなければならない。
+    if right > 0 and left < right:
+        dfs(s + ')', left, right-1)
+
+if n%2 == 0:
+    dfs('', n//2, n//2)
