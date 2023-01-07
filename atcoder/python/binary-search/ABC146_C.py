c9a086313fae4d9ce335bc6127ad4ec79ef83e81
yuki kageyama <kageyamaforwork@gmail.com>
2023-01-07 11:43:29 +0900

    二分探索

--- atcoder/python/binary-search/ABC146_C.py
+++ atcoder/python/binary-search/ABC146_C.py
@@ -1,1 +1,20 @@
+# 方針 : 二分探索
+# 答えがある一つの値に決まることがわかっているので、二分探索を行う
 
+a, b, x = map(int, input().split())
+
+
+def calc(n):
+    return a * n + b * len(str(n))
+
+
+ok, ng = 0, 10 ** 9 + 1
+while ok + 1 < ng:
+    # print(ok, ng)
+    mid = (ok + ng) // 2
+    if calc(mid) <= x:
+        ok = mid
+    else:
+        ng = mid
+
+print(ok)
